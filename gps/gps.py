64c4692ab63cdf33fe5e27c5481dd66fdc7fd9ec
sakusaku3939 <yuukiyuuki223@gmail.com>
2025-01-23 18:30:46 +0900

    [wip] 1秒待機を一次敵に削除

--- gps/gps.py
+++ gps/gps.py
@@ -1,73 +1,27 @@
-import threading
-import time
 from gps3 import gps3
-from queue import Queue
 
 
-class GPS:
-    def __init__(self):
-        self.gps_socket = gps3.GPSDSocket()
-        self.data_stream = gps3.DataStream()
-        self.running = False
-        self.data_queue = Queue(maxsize=1)  # 最新データのみ保持
-
-    def start(self):
-        """GPSデータの取得を開始する"""
-        self.running = True
-        self.gps_socket.connect()
-        self.gps_socket.watch()
-        threading.Thread(target=self._read_data, daemon=True).start()
-
-    def _read_data(self):
-        """バックグラウンドでGPSデータを取得する"""
-        last_lat, last_lng = None, None  # 最後に取得した値を記録
-        while self.running:
-            try:
-                new_data = self.gps_socket.next()
-                if new_data:
-                    self.data_stream.unpack(new_data)
-                    latitude = self.data_stream.TPV.get('lat', 'n/a')
-                    longitude = self.data_stream.TPV.get('lon', 'n/a')
-                    time_gps = self.data_stream.TPV.get('time', 'n/a')
-
-                    # データが有効であり、更新されている場合のみキューに追加
-                    if latitude != 'n/a' and longitude != 'n/a' and (latitude != last_lat or longitude != last_lng):
-                        last_lat, last_lng = latitude, longitude
-                        if self.data_queue.full():  # キューが満杯の場合、古いデータを破棄
-                            self.data_queue.get()
-                        self.data_queue.put((latitude, longitude, time_gps))
-            except Exception as e:
-                print(f"Error retrieving GPS data: {e}")
-            time.sleep(1)  # 次のデータ取得を待つ
-
-    def get_latest_data(self):
-        """キューから最新のGPSデータを取得する"""
-        while True:
-            try:
-                return self.data_queue.get(timeout=2)  # データが来るまで最大2秒待つ
-            except Exception:
-                print("Waiting for GPS data...")
-                continue
-
-    def stop(self):
-        """GPSデータ取得を停止する"""
-        self.running = False
-        self.gps_socket.close()
-
-
-if __name__ == '__main__':
-    gps = GPS()
-    gps.start()
+def main():
+    # gpsdソケットを接続
+    gps_socket = gps3.GPSDSocket()
+    data_stream = gps3.DataStream()
+    gps_socket.connect()
+    gps_socket.watch()
 
     try:
-        while True:
-            data = gps.get_latest_data()
-            if data:
-                lat, lng, t_gps = data
-                print(f"Latitude: {lat}, Longitude: {lng}, Time: {t_gps}")
-            else:
-                print("No new GPS data available.")
-            time.sleep(1)  # 他の処理も考慮して適切な間隔でループ
+        print("Receiving GPS data...")
+        for new_data in gps_socket:
+            if new_data:
+                data_stream.unpack(new_data)
+                latitude = data_stream.TPV['lat']
+                longitude = data_stream.TPV['lon']
+                altitude = data_stream.TPV['alt']
+
+                # データが取得できた場合に出力
+                print(f"Latitude: {latitude}, Longitude: {longitude}, Altitude: {altitude}")
     except KeyboardInterrupt:
-        gps.stop()
-        print('!!FINISH!!')
+        print("\n終了します")
+
+
+if __name__ == "__main__":
+    main()
